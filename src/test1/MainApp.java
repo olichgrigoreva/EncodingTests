17a09c857e47f0fae280ece64cf29ce376f37eab
Olga <aboutrocky94@gmail.com>
2020-09-08 19:54:38 +0300

    Тест 1. HashMap

--- src/test1/MainApp.java
+++ src/test1/MainApp.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class MainApp {
